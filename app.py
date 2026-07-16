27fda6c407b1b3f04bd72deba16e909489600ea5
tecbm <operations@bhayajimercantile.com>
2026-07-16 18:15:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import json
 from io import BytesIO
-from flask import Flask, request, render_template, string_to_bytes # standard components
+from flask import Flask, request, render_template
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
